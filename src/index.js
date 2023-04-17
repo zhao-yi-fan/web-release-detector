fd1126e8b91443a13615ecc083d29a7ac628c04d
zhaoyifan <zhaoyifan@fehorizon.com>
2023-04-17 19:58:28 +0800

    feat: 增加容器 自定义dom方法

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 let lastSrcs; // 上次获取到的script链接
 
-const scriptReg = /<script.*?src=["']?(?<src>[^"'\s>]+)/gm;
+// <script src=static/js/chunk-elementUI.7c48a9d2.js></script>
+// <script src='static/js/chunk-elementUI.7c48a9d2.js'></script>
+// <script src="static/js/chunk-elementUI.7c48a9d2.js"></script>
+const scriptReg = /<script[^>]*src=["']?(?<src>[^"'\s>]+)/gm;
 /**
  * 获取最新页面中的script链接
  */
@@ -27,6 +30,93 @@ async function extractNewScripts (options) {
   }
   return result;
 }
+// 生成页面的dom
+function createRefreshDom (options = {}) {
+  const container = options.container || document.body;
+
+  const div = document.createElement("div");
+  div.style.cssText = `
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 50px;
+    background-color: #fff;
+    z-index: 9999;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    transition: transform 0.5s ease-in-out;
+    box-shadow: 1px 1px 7px #ccc;
+  `;
+  div.innerHTML = `
+    <div style="
+      font-size: 14px;
+      margin-right: 10px;
+      color: #333;
+    if (document.visibilityState === "hidden") {
+      document.addEventListener("visibilitychange", async () => {
+        if (document.visibilityState === "visible") {
+          const willUpdate = await needUpdate(options);
+          if (willUpdate) {
+            callback && callback();
+          }
+        }
+      });
+      return;
+    }
+    const willUpdate = await needUpdate(options);
+    if (willUpdate) {
+      callback && callback();
+    }
+    releaseInspect(options);
+  }
+    ">检测到新版本</div>
+    <div style="
+      font-size: 14px;
+      margin-right: 10px;
+      color: #333;
+    ">请点击刷新按钮</div>
+    <div style="
+      width: 40px;
+      height: 25px;
+      border-radius: 5px;
+      background-color: #1989fa;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #fff;
+      font-size: 14px;
+      cursor: pointer;
+    " id="releaseInspectRefreshBtn">刷新</div>
+    <div style="
+      margin-left: 5px;
+      width: 40px;
+      height: 25px;
+      border-radius: 5px;
+      background-color: #1989fa;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #fff;
+      font-size: 14px;
+      cursor: pointer;
+    " id="releaseInspectCloseBtn">关闭</div>
+  `;
+  container.appendChild(div);
+  const btn = div.querySelector("#releaseInspectRefreshBtn");
+  btn.addEventListener("click", () => {
+    window.location.reload();
+  });
+  const closeBtn = div.querySelector("#releaseInspectCloseBtn");
+  closeBtn.addEventListener("click", () => {
+    div.style.transform = `translateY(-100%)`;
+    setTimeout(() => {
+      container.removeChild(div);
+    }, 500);
+  });
+}
 
 async function needUpdate (options) {
   const newScripts = await extractNewScripts(options);
@@ -47,78 +137,29 @@ async function needUpdate (options) {
   lastSrcs = newScripts;
   return result;
 }
+
+/**
+ * 
+ * @param {*} options  {
+ * DURATION: 120 * 1000, // 检测间隔时间
+ * callback: () => {}, // 检测到更新后的回调
+ * container: document.body, // 插入的容器
+ * gateway: 'http://localhost:8080/product/' // 网关
+ * customCreateDom: (options) => {} // 自定义创建dom的方法
+ * }
+ */
 export default function releaseInspect (options = {}) {
   const { callback, DURATION = 120 * 1000 } = options;
   setTimeout(async () => {
     const willUpdate = await needUpdate(options);
     if (willUpdate) {
-      const div = document.createElement("div");
-      div.style.cssText = `
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        height: 50px;
-        background-color: #fff;
-        z-index: 9999;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: row;
-        transition: transform 0.5s ease-in-out;
-        box-shadow: 1px 1px 7px #ccc;
-      `;
-      div.innerHTML = `
-        <div style="
-          font-size: 14px;
-          margin-right: 10px;
-          color: #333;
-        ">检测到新版本</div>
-        <div style="
-          font-size: 14px;
-          margin-right: 10px;
-          color: #333;
-        ">请点击刷新按钮</div>
-        <div style="
-          width: 40px;
-          height: 25px;
-          border-radius: 5px;
-          background-color: #1989fa;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          color: #fff;
-          font-size: 14px;
-          cursor: pointer;
-        " id="releaseInspectRefreshBtn">刷新</div>
-        <div style="
-          margin-left: 5px;
-          width: 40px;
-          height: 25px;
-          border-radius: 5px;
-          background-color: #1989fa;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          color: #fff;
-          font-size: 14px;
-          cursor: pointer;
-        " id="releaseInspectCloseBtn">关闭</div>
-      `;
-      // TODO 不应该加到body上，应该加到一个固定的容器上
-      document.body.appendChild(div);
-      const btn = div.querySelector("#releaseInspectRefreshBtn");
-      btn.addEventListener("click", () => {
-        window.location.reload();
-      });
-      const closeBtn = div.querySelector("#releaseInspectCloseBtn");
-      closeBtn.addEventListener("click", () => {
-        div.style.transform = `translateY(-100%)`;
-        setTimeout(() => {
-          document.body.removeChild(div);
-        }, 500);
-      });
-      callback && callback();
+      // 生成自定义dom
+      if (options.customCreateDom) {
+        options.customCreateDom(options);
+      } else {
+        createRefreshDom(options);
+      }
+      callback && callback(options);
     }
     releaseInspect(options);
   }, DURATION);
